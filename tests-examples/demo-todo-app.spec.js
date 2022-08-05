b1201148799afa5c6890216477242de76335ed50
Akhmadjon Ortikov (Adam) <105177864+Adam-CZPG@users.noreply.github.com>
2022-08-05 03:53:18 +0200

    git

--- tests-examples/demo-todo-app.spec.js
+++ tests-examples/demo-todo-app.spec.js
@@ -3,14 +3,15 @@ const { test, expect } = require('@playwright/test');
 const { globalTimeout } = require('../tests/example.spec');
 
 test ('Grinfer Test', async ({page})=>{
-page.goto('https://grinfer.com/')
-page.locator('.sc-2aalce-2').click()
-page.locator('#email'),fill ('test@gmail.com')
-page.locator('#password'),fill ('A1254789w')
-page.locator('.sc-p4qn9p-0').click()
-expect(page.locator('.sc-2aalce-2')).toBeVisible
+await page.goto('https://grinfer.com/')
+await page.locator('.sc-2aalce-2').click()
+    page.locator('#email'),fill ('test@gmail.com')
+    page.locator('#password'),fill ('A1254789w')
+await page.locator('.sc-p4qn9p-0').click()
 
-page.screenshot({ path:'screenshot/LogIn.jpg' });
+expect(page.locator('.sc-2aalce-2')).toBevisible 
+
+await page.screenshot({ path:'screenshot/LogIn.jpg' });
 
 
 });
